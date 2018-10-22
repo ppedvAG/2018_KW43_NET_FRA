3d03357eeadd3422c56c7f58c3066151de1aa3a0
André <andrerrr@outlook.de>
2018-10-22 17:06:23 +0200

    Hallo Entity Framework

---
+++ HalloEF/HalloEF/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HalloEF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        Model1Container context = new Model1Container();
+
+        private void Laden(object sender, RoutedEventArgs e)
+        {
+            myGrid.ItemsSource = context.PersonSet
+                                        .OfType<Mitarbeiter>()
+                                        .Where(x => x.Name.StartsWith("F") &&
+                                                    x.GebDatum.Month == DateTime.Now.Month)
+                                        .OrderBy(x => x.Abteilung.Sum(y => y.Bezeichnung.Length))
+                                        .ToList();
+        }
+
+        private void Demo(object sender, RoutedEventArgs e)
+        {
+            var abt1 = new Abteilung() { Bezeichnung = "Holz" };
+            var abt2 = new Abteilung() { Bezeichnung = "Steine" };
+
+            for (int i = 0; i < 100; i++)
+            {
+                var m = new Mitarbeiter()
+                {
+                    Name = $"Fred #{i:000}",
+                    Beruf = "Macht dinge",
+                    GebDatum = DateTime.Now.AddYears(-40).AddDays(17 * i)
+                };
+
+                if (i % 2 == 0)
+                    m.Abteilung.Add(abt1);
+
+                if (i % 3 == 0)
+                    m.Abteilung.Add(abt2);
+
+                context.PersonSet.Add(m);
+            }
+            context.SaveChanges();
+        }
+
+        private void Speichern(object sender, RoutedEventArgs e)
+        {
+            context.SaveChanges();
+        }
+    }
+}
